fe1c4ee31f2a315352c397d8ee5fb7f90304c9bb
vsC0dex <max071010@gmail.com>
2022-07-15 20:17:50 +0300

    add-desktop-styles

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,8 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
-
 (() => {
   const refs = {
-    openModalBtn: document.querySelector(".data-modal-open"),
-    closeModalBtn: document.querySelector(".data-modal-close"),
-    modal: document.querySelector(".data-modal"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector(".data-modal-open-mobile"),
-    closeModalBtn: document.querySelector(".data-modal-close"),
-    modal: document.querySelector(".data-modal-mobile"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
